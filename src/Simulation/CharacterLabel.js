397e251e8c33c498f40ccf71bdd6a33f1607138d
Dan Manning <dmanning@oceaneering.com>
2024-04-01 10:27:19 -0500

    ui cleanup

--- src/Simulation/CharacterLabel.js
+++ src/Simulation/CharacterLabel.js
@@ -30,8 +30,11 @@ const CharacterLabel = (props) => {
                 </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    
                     <img src={`https://dyifmflum502e.cloudfront.net/${props.agent.agentDescription.portraitFilename}`}
-                        style={{height: "50%", width: "50%"}}/>
+                        style={{height: "50%", width: "50%"}}
+                        width={256}
+                        height={384} />
                     <div>
                         {props.agent.description}
                     </div>
