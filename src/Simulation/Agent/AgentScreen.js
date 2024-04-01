85538f89a035c80aad3e9255c423ed65c2aa74ec
Dan Manning <dmanning@oceaneering.com>
2024-04-01 11:07:48 -0500

    cleaning up tabs

--- src/Simulation/Agent/AgentScreen.js
+++ src/Simulation/Agent/AgentScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import GeneralTab from './GeneralTab';
 
 const AgentScreen = (props) => {
 
@@ -11,13 +13,7 @@ const AgentScreen = (props) => {
             </TabList>
 
             <TabPanel>
-            <img src={`https://dyifmflum502e.cloudfront.net/${props.agent.agentDescription.portraitFilename}`}
-                        style={{height: "50%", width: "50%"}}
-                        width={256}
-                        height={384} />
-            <div>
-                {props.agent.description}
-            </div>
+                <GeneralTab agent={props.agent}/>
             </TabPanel>
             <TabPanel>
                 <h2>Any content 2</h2>
