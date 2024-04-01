fa738a3bd63e292c78745dc4ace9d2faf5806aa4
Dan Manning <dmanning@oceaneering.com>
2024-04-01 11:03:59 -0500

    added tabs to agent screen

--- src/Simulation/Agent/AgentScreen.js
+++ src/Simulation/Agent/AgentScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const AgentScreen = (props) => {
 
     return (
-        <div >
+        <Tabs>
+            <TabList>
+                <Tab>General</Tab>
+                <Tab>Description</Tab>
+            </TabList>
+
+            <TabPanel>
             <img src={`https://dyifmflum502e.cloudfront.net/${props.agent.agentDescription.portraitFilename}`}
                         style={{height: "50%", width: "50%"}}
                         width={256}
@@ -11,7 +18,11 @@ const AgentScreen = (props) => {
             <div>
                 {props.agent.description}
             </div>
-        </div>
+            </TabPanel>
+            <TabPanel>
+                <h2>Any content 2</h2>
+            </TabPanel>
+        </Tabs>
     );
 }
 
