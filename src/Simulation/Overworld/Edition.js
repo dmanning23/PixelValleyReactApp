e663e4622d875d1c61dbed42dad6087c9dad535a
Dan Manning <dmanning@oceaneering.com>
2024-05-14 17:23:45 -0500

    newspaper stuff

--- src/Simulation/Overworld/Edition.js
+++ src/Simulation/Overworld/Edition.js
@@ -23,15 +23,31 @@ const Edition = (props) => {
         return (
             <div>
                 <h1>{props.newspaperName}</h1>
-                <h1>{data.edition.edition.date}</h1>
-                {data.edition.edition.articles.map((data, index) => {
-                    return (
-                        <div key={index} className="border border-dark">
-                            <h3>{data.headline}</h3>
-                            <p>{data.body}</p>
+                <h3>{data.edition.edition.date}</h3>
+                <div className="accordion" id="accordionExample">
+                    {data.edition.edition.articles.map((data, index) => {
+                        return <div key={index} className="accordion-item">
+                            <h4 className="accordion-header" id={`heading${index}`} >
+                                <button className="accordion-button"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target={`#collapse${index}`}
+                                    aria-expanded="false"
+                                    aria-controls={`collapse${index}`}>
+                                    <h4>{data.headline}</h4>
+                                </button>
+                            </h4>
+                            <div id={`collapse${index}`}
+                                className="accordion-collapse collapse show"
+                                aria-labelledby={`heading${index}`}
+                                data-bs-parent="#accordionExample" >
+                                <div className="accordion-body">
+                                    <p>{data.body}</p>
+                                </div>
+                            </div>
                         </div>
-                    );
-                })}
+                    })}
+                </div>
             </div>
         )
     }
